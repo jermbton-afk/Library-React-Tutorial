4764a4863c3703d7633050c32b041fbe02048f5c
Jeremiah <jermbton@gmail.com>
2026-08-09 03:26:57 -0400

    last push

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,23 +1,59 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Price from "./Price";
+import Rating from "./Rating";
 
 const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(true);
+
+  useEffect(() => {
+    mountedRef.current = true;
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        console.log("setting image", mountedRef.current);
+        if (mountedRef.current) {
+          setImg(image);
+        }
+      }, 300);
+    };
+    return () => {
+      mountedRef.current = false;
+    }
+  }, [book.url]);
+
   return (
     <div className="book">
-      <Link to={`/books/${book.id}`}>
-        <figure className="book__img--wrapper">
-          <img src={book.url} alt="" className="book__img" />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book_-title--link">
-          {book.title}
-        </Link>
-      </div>
-      <rating rating={book.rating} />
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/>
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img src={img.src} alt="" className="book__img" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book_-title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice}
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/ui/Rating.jsx
+++ src/components/ui/Rating.jsx
@@ -8,6 +8,7 @@ const Rating = ({ rating }) => {
         <FontAwesomeIcon icon="star" key={index} />
       ))}
       {!Number.isInteger(rating) && <FontAwesomeIcon icon="star-half-alt" />}
+    
     </div>
   );
 };
